e0600c0ad9b0f0c1a82c070ba7b063e89fb584d4
ALEX12345432 <61266043+ALEX12345432@users.noreply.github.com>
2020-02-23 19:08:51 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-$('.menu-btn').on('click', function(){
-  $(this).toggleClass('menu-btn_active');
-  $('.menu-nav').toggleClass('menu-nav_active');
+$(function() {
+    $('.dragged-item').draggable();
+    $('.menu-btn').on('click', function() {
+        $(this).toggleClass('menu-btn_active');
+        $('.menu-nav').toggleClass('menu-nav_active');
+    });
+    $('.card_item').on('mousemove', function(e) {
+        $(this).css(
+            'transform', 'rotateX(' + (e.offsetY - $(this).height() / 2) / 2 + 'deg) rotateY(' + -(e.offsetX - $(this).height() / 2) / 2 + 'deg)'
+        );
+    });
+    $('.card_item').on('mousemove', function(e) {
+        $(this).css(
+            'transform', 'rotateX(' + -(e.offsetY / 5 - $('.card_-item').height() / 2) / 4 + 'deg) rotateY(' + (e.offsetX / 5 - $(this).height() / 2) / 4 + 'deg)'
+        );
+    });
+    $('.card_item').on('mouseout', function(e) {
+        $(this).css('transform','rotateX(0) rotateY(0)');
+    });
 });
